408c7730b6be5156a73810913eb543f0027ec277
Nadun Dhananjaya <nadundhananjaya98@yahoo.com>
2023-05-05 00:16:12 +0530

    update routes

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Login/>,
+        element: <FlightList/>,
       },
       //   Flight Routes
       {
